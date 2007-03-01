7ed214d7c0e36165505a9943f64ba4a0d1145e03
Michael Kaply <consulting@kaply.com>
2007-03-01 18:24:48 +0000

    Wrong paths

--- buildit.cmd
+++ buildit.cmd
@@ -2,7 +2,8 @@ del operator.xpi
 copy chrome.manifest.jar chrome.manifest
 cd chrome
 del operator.jar
-zip -r operator.jar content locale
-cd ..
+cd operator
+zip -r ..\operator.jar content locale
+cd ..\..
 zip operator.xpi chrome\operator.jar chrome.manifest defaults\preferences\prefs.js install.rdf LICENSE
 copy chrome.manifest.flat chrome.manifest
