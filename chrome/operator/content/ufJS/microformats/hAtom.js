61c6eb2afbd362b2b1f20ef2b371671c37a1fc75
Michael Kaply <consulting@kaply.com>
2007-03-01 20:01:59 +0000

    Better hAtom support - separate hEntry and hFeed into their own mini microformats for internal representation

--- chrome/operator/content/ufJS/microformats/hAtom.js
+++ chrome/operator/content/ufJS/microformats/hAtom.js
@@ -1,114 +1,63 @@
 /*extern ufJSParser */
+/* hAtom is kind of strange. I decided to separate it in to hFeed and hEntry */
 
-function hAtom() {
+function hFeed() {
 }
 
-ufJSParser.microformats.hAtom = {
+function hEntry() {
+}
+
+ufJSParser.microformats["hAtom-hEntry"] = {
   version: "0.2",
-  mfName: "hAtom",
-  mfObject: hAtom,
-  className: "hfeed",
-  alternateClass: "hentry",
-  children: "hentry",
-  required: ["entry-title", "updated"],
+  mfName: "hAtom-hEntry",
+  mfObject: hEntry,
+  className: "hentry",
   definition: {
     properties: {
-      "hentry": {
-        subproperties: {
-          "author" : {
-            value: [],
-            getter: function(propnode, mfnode, definition) {
-              return ufJSParser.createMicroformat(propnode, "hCard");
-            }
-          },
-          "bookmark" : {
-            value: "",
-            rel: true,
-            getter: function(propnode, mfnode, definition) {
-              return ufJSParser.createMicroformat(propnode, "bookmark");
-            }
-          },
-          "entry-title" : {
-            value: "",
-          },
-          "entry-content" : {
-            value: [],
-          },
-          "entry-summary" : {
-            value: [],
-          },
-          "published" : {
-            value: "",
-          },
-          "updated" : {
-            value: "",
-          },
-          "tag" : {
-            value: "",
-            rel: true,
-            getter: function(propnode, mfnode, definition) {
-              return ufJSParser.createMicroformat(propnode, "tag");
-            }
-          }
-        },
+      "author" : {
         value: [],
-        ufjs: {
-          "ufjsChildDisplayName" : {
-            value: "",
-            virtual: true,
-            getter: function(propnode, mfnode, definition) {
-              return ufJSParser.getMicroformatProperty(mfnode, "hAtom", "entry-title");
-            }
-          }
+        getter: function(propnode, mfnode, definition) {
+          return ufJSParser.createMicroformat(propnode, "hCard");
         }
       },
-      "tag" : {
-        value: [],
+      "bookmark" : {
+        value: "",
         rel: true,
         getter: function(propnode, mfnode, definition) {
-          var tags = ufJSParser.getElementsByAttribute(mfnode, "rel", "tag");
-          var tagArray = [];
-          var i;
-          var xpathExpression = "ancestor::*[contains(concat(' ', @class, ' '), ' hentry ')]";
-          var docforxpath;
-          if (!mfnode.ownerDocument) {
-            docforxpath = mfnode;
-          } else {
-            docforxpath = mfnode.ownerDocument;
-          }
-          for (i = 0; i < tags.length; i++) {
-            var xpathResult = docforxpath.evaluate(xpathExpression, tags[i], null,  XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-            if (xpathResult.singleNodeValue) {
-              continue;
-            } else {
-              tagArray.push(tags[i]);
-            }
-          }
-          if (tagArray.length > 0) {
-            return tagArray;
-          }
+          return ufJSParser.createMicroformat(propnode, "bookmark");
         }
-      }
-    },
-    values: {
-      "text" : {
+      },
+      "entry-title" : {
         value: "",
-        virtual: true,
+      },
+      "entry-content" : {
+        value: [],
+      },
+      "entry-summary" : {
+        value: [],
+      },
+      "published" : {
+        value: "",
+      },
+      "updated" : {
+        value: "",
+      },
+      "tag" : {
+        value: "",
+        rel: true,
         getter: function(propnode, mfnode, definition) {
-          if (!mfnode.ownerDocument) {
-            return mfnode.title;
-          } else {
-            return mfnode.ownerDocument.title;
-          }
+          return ufJSParser.createMicroformat(propnode, "tag");
         }
       }
     },
+    value: [],
     ufjs: {
       "ufjsDisplayName" : {
         value: "",
         virtual: true,
         getter: function(propnode, mfnode, definition) {
-          return ufJSParser.getMicroformatProperty(mfnode, "hAtom", "text");
+          /* NEEDS MORE WORK */
+          return ufJSParser.getMicroformatProperty(mfnode, "hAtom-hEntry", "entry-title");
         }
       }
     },
@@ -141,3 +90,87 @@ ufJSParser.microformats.hAtom = {
   }
 };
 
+ufJSParser.microformats["hAtom-hFeed"] = {
+  version: "0.2",
+  mfName: "hAtom-Feed",
+  mfObject: hFeed,
+  className: "hfeed",
+  alternateClassName: "hentry",
+  definition: {
+    properties: {
+      "author" : {
+        value: [],
+        getter: function(propnode, mfnode, definition) {
+          return ufJSParser.createMicroformat(propnode, "hCard");
+        }
+      },
+      "tag" : {
+        value: [],
+        rel: true,
+        getter: function(propnode, mfnode, definition) {
+          var tags = ufJSParser.getElementsByAttribute(mfnode, "rel", "tag");
+          var tagArray = [];
+          var i;
+          var xpathExpression = "ancestor::*[contains(concat(' ', @class, ' '), ' hentry ')]";
+          var docforxpath;
+          if (!mfnode.ownerDocument) {
+            docforxpath = mfnode;
+          } else {
+            docforxpath = mfnode.ownerDocument;
+          }
+          for (i = 0; i < tags.length; i++) {
+            var xpathResult = docforxpath.evaluate(xpathExpression, tags[i], null,  XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+            if (xpathResult.singleNodeValue) {
+              continue;
+            } else {
+              tagArray.push(tags[i]);
+            }
+          }
+          if (tagArray.length > 0) {
+            return tagArray;
+          }
+        }
+      }      
+    },
+    value: [],
+    ufjs: {
+      "ufjsDisplayName" : {
+        value: "",
+        virtual: true,
+        getter: function(propnode, mfnode, definition) {
+          if (mfnode.ownerDocument) {
+            return mfnode.ownerDocument.title;
+          } else {
+            return mfnode.title;
+          }
+        }
+      }
+    },
+    defaultGetter: function(propnode) {
+      if ((propnode.nodeName.toLowerCase() == "abbr") && (propnode.getAttribute("title"))) {
+        return propnode.getAttribute("title");
+      } else if ((propnode.nodeName.toLowerCase() == "img") && (propnode.getAttribute("alt"))) {
+        return propnode.getAttribute("alt");
+      } else if ((propnode.nodeName.toLowerCase() == "area") && (propnode.getAttribute("alt"))) {
+        return propnode.getAttribute("alt");
+      } else {
+        var values = ufJSParser.getElementsByClassName(propnode, "value");
+        if (values.length > 0) {
+          var value = "";
+          for (var j=0;j<values.length;j++) {
+            value += values[j].textContent;
+          }
+          return value;
+        } else {
+          var s;
+          if (propnode.innerText) {
+            s = propnode.innerText;
+          } else {
+            s = propnode.textContent;
+          }
+          return ufJSParser.trim(s);
+        }
+      }
+    }
+  }
+};
