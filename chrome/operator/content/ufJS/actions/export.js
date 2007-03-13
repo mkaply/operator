112d35c7bc5c482fa982b1feef14175ac82c3888
Michael Kaply <consulting@kaply.com>
2007-03-13 01:58:40 +0000

    Add support for operations against all microformats

--- chrome/operator/content/ufJS/actions/export.js
+++ chrome/operator/content/ufJS/actions/export.js
@@ -2,9 +2,52 @@
 
 ufJSActions.actions.export_vcard = {
   description: "Export Contact",
+  descriptionAll: "Export All",
   scope: {
     microformats: {
       "hCard" : "hCard"
+    }
+  },
+  doActionAll: function(doc, microformatName, event) {
+    var microformatNames;
+    microformatNames = [];
+    microformatNames.push(microformatName);
+    var url;
+    for (var i in microformatNames) {
+      if (microformatNames[i] == "hCard") {
+        var vcards = ufJSParser.getElementsByClassName(doc, "vcard");
+        if (vcards.length > 0) {
+          var file = Components.classes["@mozilla.org/file/directory_service;1"].
+                                getService(Components.interfaces.nsIProperties).
+                                get("TmpD", Components.interfaces.nsIFile);
+      
+          file.append("hCard.vcf");
+      
+          var fos = Components.classes["@mozilla.org/network/file-output-stream;1"].
+                               createInstance(Components.interfaces.nsIFileOutputStream);
+      
+          fos.init(file, -1, -1, false);
+        }
+        for (var j =0; j < vcards.length; j++) {
+          var vcf = ufJS.vCard(vcards[j]);
+          if (vcf) {
+            fos.write(vcf, vcf.length);
+          }
+        }
+        fos.close();                                                                  
+    
+        var f = Components.classes["@mozilla.org/file/local;1"].
+                           createInstance(Components.interfaces.nsILocalFile);
+        f.initWithPath(file.path);
+        url = Components.classes["@mozilla.org/network/io-service;1"].
+                         getService(Components.interfaces.nsIIOService).
+                         newFileURI(f).
+                         spec;
+        break;
+      }
+    }
+    if (url) {
+      openUILink(url, event);
     }
   },
   doAction: function(node, microformatName, event) {
