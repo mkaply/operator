fb0260ad723243166725b62010989c008e0df7e6
Michael Kaply <consulting@kaply.com>
2007-03-19 18:54:35 +0000

    Moving second tier microformats and actions to userscripts

---
+++ userscripts/GRDDL.js
@@ -0,0 +1,71 @@
+/*extern ufJSParser, ufJS, ufJSActions, openUILink */ 
+
+function GRDDL() {
+}
+
+ufJSParser.microformats.GRDDL = {
+  version: "0.2",
+  mfName: "GRDDL",
+  mfObject: GRDDL,
+  attributeName: "rel",
+  attributeValues: ["transformation"],
+  definition: {
+    values: {
+      "transformation" : {
+        value: "",
+        virtual: true,
+        getter: function(propnode, mfnode, definition) {
+          return "foo";
+        }
+      }
+    },
+    ufjs: {
+      "ufjsDisplayName" : {
+        value: "",
+        virtual: true,
+        getter: function(propnode, mfnode, definition) {
+          if (mfnode.ownerDocument) {
+            return mfnode.ownerDocument.title;
+          } else {
+            return mfnode.title;
+          }
+        }
+      }
+    }
+  }
+};
+
+ufJSActions.actions.extract_rdf = {
+  description: "Extract RDF",
+  scope: {
+    microformats: {
+      "GRDDL" : "GRDDL"
+    }
+  },
+  doAction: function(node, microformatName, event) {
+    var microformatNames;
+    if (!microformatName) {
+      microformatNames = ufJS.getMicroformatNameFromNode(node);
+    } else {
+      microformatNames = [];
+      microformatNames.push(microformatName);
+    }
+    var url;
+    for (var i in microformatNames) {
+      if (microformatNames[i] == "GRDDL") {
+        url = "http://www.w3.org/2005/08/online_xslt/xslt?xslfile=http%3A%2F%2Fwww.w3.org%2F2003%2F11%2Frdf-in-xhtml-processor&xmlfile=";
+        var pageurl;
+        if (node.ownerDocument) {
+          pageurl = node.ownerDocument.URL;
+        } else {
+          pageurl = node.URL;
+        }
+        url += encodeURIComponent(pageurl);
+        break;
+      }
+    }
+    if (url) {
+      openUILink(url, event);
+    }
+  }
+};
---
+++ userscripts/species.js
@@ -0,0 +1,102 @@
+/*extern ufJSParser, ufJS, ufJSActions, openUILink */ 
+
+
+
+function species() {
+}
+
+ufJSParser.microformats.species = {
+  version: "0.2",
+  mfName: "species",
+  mfObject: species,
+  className: "biota",
+  definition: {
+    properties: {
+      "domain" : {
+        value: ""
+      },
+      "kingdom" : {
+        value: ""
+      },
+      "subkingdom" : {
+        value: ""
+      },
+      "superphylum" : {
+        value: ""
+      },
+      "vernacular" : {
+        value: ""
+      },
+      "binomial" : {
+        value: ""
+      }
+    },
+    ufjs: {
+      "ufjsDisplayName" : {
+        value: "",
+        virtual: true,
+        getter: function(propnode, mfnode, definition) {
+          return ufJSParser.getMicroformatProperty(mfnode, "species", "vernacular");
+        }
+      }
+    },
+    defaultGetter: function(propnode) {
+      if (((propnode.nodeName.toLowerCase() == "abbr") || (propnode.nodeName.toLowerCase() == "html:abbr")) && (propnode.getAttribute("title"))) {
+        return propnode.getAttribute("title");
+      } else if ((propnode.nodeName.toLowerCase() == "img") && (propnode.getAttribute("alt"))) {
+        return propnode.getAttribute("alt");
+      } else if ((propnode.nodeName.toLowerCase() == "area") && (propnode.getAttribute("alt"))) {
+        return propnode.getAttribute("alt");
+      } else {
+        var values = ufJSParser.getElementsByClassName(propnode, "value");
+        if (values.length > 0) {
+          var value = "";
+          for (var j=0;j<values.length;j++) {
+            value += values[j].textContent;
+          }
+          return value;
+        } else {
+          var s;
+          if (propnode.innerText) {
+            s = propnode.innerText;
+          } else {
+            s = propnode.textContent;
+          }
+          return ufJSParser.trim(s);
+        }
+      }
+    }
+  }
+};
+
+ufJSActions.actions.wikispecies_search = {
+  description: "Wikispecies",
+  icon: "http://species.wikimedia.org/favicon.ico",
+  scope: {
+    microformats: {
+      "species" : "binomial"
+    }
+  },
+  doAction: function(node, microformatName, event) {
+    var microformatNames;
+    if (!microformatName) {
+      microformatNames = ufJS.getMicroformatNameFromNode(node);
+    } else {
+      microformatNames = [];
+      microformatNames.push(microformatName);
+    }
+    var url;
+    var action = ufJSActions.actions.wikispecies_search;
+    for (var i in microformatNames) {
+      var param = ufJSParser.getMicroformatProperty(node, microformatNames[i], action.scope.microformats[microformatNames[i]]);
+      if (param) {
+        url = "http://species.wikimedia.org/wiki/Special:Search?search=" + encodeURIComponent(param) + "&go=Go";
+        break;
+      }
+    }
+    if (url) {
+      openUILink(url, event);
+    }
+  }
+};
+
