fa95ad106f30231c4b16d30bb0e6e8119beeb6f9
Michael Kaply <consulting@kaply.com>
2007-05-24 16:31:18 +0000

    Package all actions into one file

--- buildit.cmd
+++ buildit.cmd
@@ -3,7 +3,11 @@ copy chrome.manifest.jar chrome.manifest
 cd chrome
 del operator.jar
 cd operator
-zip -r ..\operator.jar content locale
+cd content
+cd actions
+copy * ..\actions.js /B
+cd ..\..
+zip ..\operator.jar content locale
 cd ..\..
 zip operator.xpi chrome\operator.jar chrome.manifest defaults\preferences\prefs.js install.rdf LICENSE
 copy chrome.manifest.flat chrome.manifest
