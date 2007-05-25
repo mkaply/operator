a311fc5d6c4d8d0e6c4dfa96f41b985d83abec61
Michael Kaply <consulting@kaply.com>
2007-05-25 21:51:25 +0000

    Handle . in semantic scope

--- userscripts/goto_url.js
+++ userscripts/goto_url.js
@@ -9,7 +9,14 @@ var goto_url = {
     }
   },
   doAction: function(semanticObject, semanticObjectType) {
-    var url = semanticObject[this.scope.semantic[semanticObjectType]];
+    var url;
+    var property = this.scope.semantic[semanticObjectType];
+    if (property.indexOf(".") != -1) {
+      var props = property.split(".");
+      url = semanticObject[props[0]][props[1]];
+    } else {
+      url = semanticObject[property];
+    }
     if (url) {
       if (url instanceof Array) {
         return url[0];
