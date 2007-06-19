ad0cd77c197e568a77c0ea411feb376f60499c8c
Michael Kaply <consulting@kaply.com>
2007-06-19 21:42:48 +0000

    support new property Index

--- userscripts/goto_url.js
+++ userscripts/goto_url.js
@@ -9,7 +9,7 @@ var goto_url = {
       "XFN" : "link"
     }
   },
-  doAction: function(semanticObject, semanticObjectType) {
+  doAction: function(semanticObject, semanticObjectType, propertyIndex) {
     var url;
     var property = this.scope.semantic[semanticObjectType];
     if (property.indexOf(".") != -1) {
@@ -20,12 +20,41 @@ var goto_url = {
     }
     if (url) {
       if (url instanceof Array) {
-        return url[0];
+        if (propertyIndex) {
+          return url[propertyIndex];
+        } else {
+          return url[0];
+        }
       } else {
         return url;
       }
     }
-  } 
+  },
+  getActionName: function(semanticObject, semanticObjectType, propertyIndex) {
+    var url;
+    var property = this.scope.semantic[semanticObjectType];
+    if (property.indexOf(".") != -1) {
+      var props = property.split(".");
+      url = semanticObject[props[0]][props[1]];
+    } else {
+      url = semanticObject[property];
+    }
+    if (url && url instanceof Array && url.length > 1) {
+      var name = semanticObject.toString() + " (";
+      var split_url = url[propertyIndex].split(":");
+      if (split_url[0] == "aim") {
+        name += "AIM";
+      } else if (split_url[0] == "ymsgr") {
+        name += "Yahoo! Messenger";
+      } else {
+        name += url[propertyIndex];
+      }
+      name += ")";
+      return name;
+    } else {
+      return semanticObject.toString();
+    }
+  }
 };
 
 SemanticActions.add("goto_url", goto_url);
