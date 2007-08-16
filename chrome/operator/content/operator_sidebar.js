07de19560d07f2ffba3c65b235860bf79a4e6d1e
Michael Kaply <consulting@kaply.com>
2007-08-16 14:44:44 +0000

    Add message to sidebar when there no rich content

--- chrome/operator/content/operator_sidebar.js
+++ chrome/operator/content/operator_sidebar.js
@@ -33,6 +33,7 @@ var Operator_Sidebar = {
 
     var doc = document.getElementById("sidebar").contentDocument;
     var tree = doc.getElementById("operator-sidebar-tree")
+    var addedContent = false;
 
     var main_treechildren = doc.createElement("treechildren");
     main_treechildren.setAttribute("id", "operator-sidebar-treechildren");
@@ -62,6 +63,7 @@ var Operator_Sidebar = {
             sub_treerow.appendChild(sub_treecell);
             sub_treeitem.appendChild(sub_treerow);
             sub_treechildren.appendChild(sub_treeitem);
+            addedContent = true;
           }
         }
         if (addRow) {
@@ -71,6 +73,15 @@ var Operator_Sidebar = {
           main_treechildren.appendChild(main_treeitem);
         }
       }
+    }
+    if (!addedContent) {
+      var blank_treeitem = doc.createElement("treeitem");
+      var blank_treerow = doc.createElement("treerow");
+      var blank_treecell = doc.createElement("treecell");
+      blank_treecell.setAttribute("label", Operator.languageBundle.GetStringFromName("operatorSidebar.noContent"));
+      blank_treerow.appendChild(blank_treecell);
+      blank_treeitem.appendChild(blank_treerow);
+      main_treechildren.appendChild(blank_treeitem);
     }
     tree.appendChild(main_treechildren);
   },
