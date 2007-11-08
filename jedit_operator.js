b34d17c06e3c385db6941d11897928f12514ded0
Michael Kaply <consulting@kaply.com>
2007-11-08 22:15:24 +0000

    jedit defines

--- jedit_operator.js
+++ jedit_operator.js
@@ -1,16 +1,20 @@
+var tag;
 var Microformats;
 var Operator;
-var ufJS;
-var ufJSParser;
-var ufJSActions;
+var RDFa;
+var eRDF;
+var SemanticActions;
 var XMLHttpRequest;
 var XPathResult;
 var XSLTProcessor;
 var XMLSerializer;
 var hCard;
+var hCalendar;
+var xFolk;
 var Operator_Statusbar;
 var Operator_Toolbar;
 var Operator_ToolbarButton;
+var Operator_URLbarButton;
 var Operator_Sidebar;
 var gContextMenu;
 function getBrowser() {}
