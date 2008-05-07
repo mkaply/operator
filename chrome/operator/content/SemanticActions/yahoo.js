059b816490869e9e9cde8f1ebeeac9a47b344888
Michael Kaply <consulting@kaply.com>
2008-05-07 03:42:37 +0000

    upcoming.org bug only exists when there is no time specified on dtend

--- chrome/operator/content/SemanticActions/yahoo.js
+++ chrome/operator/content/SemanticActions/yahoo.js
@@ -149,10 +149,10 @@ var yahoo_calendar = {
         var dtEndDate = Microformats.dateFromISO8601(hcalendar.dtend);
         if (!dtEndDate.time) {
           dtEndDate.setDate(dtEndDate.getDate()-1);
-        }
-        if (!Operator.upcomingBugFixed) {
-          if (content.document.location.href.indexOf("http://upcoming.yahoo.com") == 0) {
-            dtEndDate.setDate(dtEndDate.getDate()+1);
+          if (!Operator.upcomingBugFixed) {
+            if (content.document.location.href.indexOf("http://upcoming.yahoo.com") == 0) {
+              dtEndDate.setDate(dtEndDate.getDate()+1);
+            }
           }
         }
         
